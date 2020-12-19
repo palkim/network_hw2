e67a117816bab184fd0c1c8299e82833c2dc4bc9
e2161420 <akcam.petek@gmail.com>
2020-12-19 21:12:54 +0300

    first commit

---
+++ client_udp.py
@@ -0,0 +1,58 @@
+import socket
+import time
+import os
+import math
+import hashlib
+
+HEADERSIZE = 100
+CHUNKSIZE = 1000
+
+
+def getChecksum(packet):
+    return hashlib.md5(packet).hexdigest()
+
+
+
+# create client UDP/IP socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# bind client to its sender port
+s.bind((socket.gethostname(), 1235))
+
+#client will connet to hostname and port number of server
+s.connect((socket.gethostname(), 1242))
+
+is_first = True
+
+file_name = "transfer_file_TCP.txt"
+file_stats = os.stat(file_name)
+
+file_size = file_stats.st_size
+number_of_chunks = math.ceil(file_size / CHUNKSIZE)
+total_time_passed = 0
+timestamp_out = 0
+chunk_number = 0
+with open(file_name, 'r') as f:
+    while True:
+        chunk_number += 1
+        if not is_first:
+            timestamp_inc = s.recv(100)
+            timestamp_inc = timestamp_inc.decode("utf-8")
+            total_time_passed += float(timestamp_inc) - float(timestamp_out)
+
+        read_data = f.read(CHUNKSIZE)
+
+        if chunk_number == number_of_chunks:
+            average_time_passed = total_time_passed / number_of_chunks
+            msg = read_data
+            timestamp_out = time.time()
+            msg = f"{len(msg)}," + f"{timestamp_out}," + f"{number_of_chunks}," + f"{average_time_passed:<{HEADERSIZE - len(str(len(msg))) - len(str(timestamp_out)) - len(str(number_of_chunks)) - 3}}" + msg
+            s.send(bytes(msg,"utf-8"))
+            break
+
+        # send data to server
+        msg = read_data
+        msg = f"{len(msg):<{HEADERSIZE}}" + msg
+        timestamp_out = time.time()
+        s.send(bytes(msg,"utf-8"))
+        if is_first:
+            is_first = False
---
+++ server_udp.py
@@ -0,0 +1,67 @@
+import socket
+import time
+import hashlib
+
+HEADERSIZE = 100
+CHUNKSIZE = 1000
+
+
+def getChecksum(packet):
+    return hashlib.md5(packet).hexdigest()
+
+# create UDP/IP socket s
+# AF_INET builds for IPv4
+# SOCKET_DGRAM builds for UDP
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# bind the socket to a port on the server machine
+# give a tuple containing host name and port number
+s.bind((socket.gethostname(), 1242))
+
+#this socket listens only for ne connection, queueing is not needed for this task
+s.listen(1)
+
+#get the file to be written
+f = open("transfer_file_UDP_server.txt", "a")
+
+# get information about the client requested to connect
+clientsocket, address = s.accept()
+
+#since we are getting the message chunk by chunk we need to concatenate them
+full_msg = ""
+
+new_msg = True
+final_chunk_flag = False
+while True:
+    msg = clientsocket.recv(CHUNKSIZE + HEADERSIZE)
+    if new_msg:
+        header = msg[:HEADERSIZE]
+        header = header.decode("utf-8")
+        header = header.split(",")
+        if len(header) == 1:
+            msglen = int(header[0])
+            timestamp = time.time()
+        elif len(header) == 4:
+            msglen = int(header[0])
+            timestamp = time.time() #time of last packet received
+            timestamp_incoming = float(header[1]) #time of the last packed sent
+            number_of_chunks = int(header[2])
+            average_transmission_time = float(header[3].strip())
+            average_transmission_time += (timestamp - timestamp_incoming) / number_of_chunks
+            final_chunk_flag = True
+        new_msg = False
+
+    full_msg += msg.decode("utf-8")
+    
+    if len(full_msg) - HEADERSIZE == msglen:
+        if final_chunk_flag == True:
+            print("TCP Packets Average Transmission Time: " + str(average_transmission_time * 1000) + " ms")
+            f.write(full_msg[HEADERSIZE:])
+            break
+        timestamp_msg = str(timestamp)
+        clientsocket.send(bytes(timestamp_msg, "utf-8"))
+        f.write(full_msg[HEADERSIZE:])
+        new_msg = True
+        full_msg = ""
+        
+f.close()
